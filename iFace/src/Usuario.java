95ee3974759d6ab823836eb2e3be4d422b61181a
Eduardo Antônio de Lucena Lisboa <38367195+EduardoLisboa@users.noreply.github.com>
2018-12-04 22:53:41 -0200

    Adição do projeto ao Git

---
+++ iFace/src/Usuario.java
@@ -0,0 +1,13 @@
+public class Usuario {
+
+    String login;
+    String senha;
+    String nome_usuario;
+
+    String nome_completo;
+    int idade;
+    String aniversario;
+
+
+
+}
---
+++ iFace/src/iFace.java
@@ -0,0 +1,9 @@
+public class iFace {
+
+    static public void main(String[] args) {
+
+        System.out.println("Rodando!");
+
+    }
+
+}
